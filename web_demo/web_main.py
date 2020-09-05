19530c6d66994849d9f1abe2f15bb57943f26310
ohyicong <ohyicong@hotmail.com>
2020-09-06 02:40:02 +0800

    web-demo

---
+++ web_demo/web_main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 08:49:23 2020
+
+@author: OHyic
+"""
+
+#import flasks libraries
+from flask import Flask, render_template, request, jsonify, url_for, redirect, flash, Response, send_file
+from flask_socketio import SocketIO, emit
+import webbrowser
+from threading import Timer
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    Timer(1,lambda: webbrowser.open_new("http://127.0.0.1:5000/")).start()
+    socketio.run(app)
